221d467988b09ed4472aa6c1f3d532fa9ab81d3c
charlie <carlos@cjbarroso.com>
2018-07-19 15:18:03 -0300

    Agregadas localizaciones (stub)!

---
+++ js/localizations.js
@@ -0,0 +1,29 @@
+String.toLocaleString({
+	"en": {
+		"%title": "English - l10n.js demo",
+		"%info": "You are viewing an English localization of this page."
+    },
+    "es": {
+        "%home_lnk" : "Inicio",
+        "%about_lnk" : "Acerca de",
+        "%services_lnk" : "Servicios", 
+        "%contact_lnk" : "Contacto",
+        "%anim_subtitle" : "AI Catalizer",
+        "%more_" : "MAS",
+        "%about_me_title" : "Algo <span>sobre</span> mí",
+        "%about_me": "Carlos es gerente de arquitectura en Machinalis, donde dirige el área de \
+        generación de soluciones, especialmente para \
+                        e-commerce y fintech. Su misión ha sido ayudar a sus clientes a entender, \
+                        planificar y ejecutar estrategias de negocios apoyadas en la inteligencia \
+                        artificial. Su experiencia en roles de gestión operativo, técnicos y de \
+                        dirección de negocios lo habilitan a tener una visión muy amplia de las \
+                        oportunidades que enfrentan sus clientes, y un criterio informado al \
+                        momento de tomar desiciones en iniciativas relacionadas con la IA.",
+        "%helping_clients" : "Como ayudo a mis clientes",
+        "%arquit_title" : "Arquitectura",
+        "%arquitectura" : "Creación de la arquitectura, selección de tecnologías y de proveedores para su iniciativa de inteligencia artificial.",
+        "%prod_adv_title" : "Product Advisory",
+    },
+});
+
+
