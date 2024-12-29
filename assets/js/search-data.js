38d62d5a2b00d43f84cc5ecce23be9bf41b58e35
cjbarroso <carlos.barroso@teracloud.io>
2024-12-29 03:46:11 +0000

    Deploying to gh-pages from @ cjbarroso/cjbarroso.github.io@fa71337ea18d588e7da4a3b481005c88b0162f62 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "My current, non-work projects",
+          description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -453,14 +453,14 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-1",
-          title: '1',
-          description: "Quantified Self",
+          section: "News",},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-agents-self-discovery",
-          title: 'Agents self-discovery',
-          description: "",
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
